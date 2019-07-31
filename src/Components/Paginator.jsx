f4e0676e62dffca5cb75abe1c79bfe3ea2f1e098
RTHJones <robin.thedevilhimself.jones@googlemail.com>
2019-07-31 20:33:50 +0100

    changed Paginator from class to const as no state required. Replaced readme node install cli instructions with link to node download page

--- src/Components/Paginator.jsx
+++ src/Components/Paginator.jsx
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Paginator extends Component {
-    render() {
-        let {showPaginator, limit, page, totalCount, changePage, prevState, handleChange} = this.props;
-        return (
-            <div className="paginator">
-                {showPaginator && <div className="pageBar">
-                    <button onClick={() => changePage(prevState, -1)} disabled={page === 1}>Previous Page</button>
-                    Page: {page}
-                    <button onClick={() => changePage(prevState, 1)} disabled={page >= totalCount / limit}>Next Page</button>
-                    <label>Results per page
-                        <select value={limit} onChange={(event) =>  handleChange(event, 'limit')}>
-                            <option value="5">5</option>
-                            <option value="10">10</option>
-                            <option value="20">20</option>
-                        </select>
-                    </label>
-                </div>}
-            </div>
-        );
-    }
+const Paginator = () => {
+    let {showPaginator, limit, page, totalCount, changePage, prevState, handleChange} = this.props;
+    return (
+        <div className="paginator">
+            {showPaginator && <div className="pageBar">
+                <button onClick={() => changePage(prevState, -1)} disabled={page === 1}>Previous Page</button>
+                Page: {page}
+                <button onClick={() => changePage(prevState, 1)} disabled={page >= totalCount / limit}>Next Page</button>
+                <label>Results per page
+                    <select value={limit} onChange={(event) =>  handleChange(event, 'limit')}>
+                        <option value="5">5</option>
+                        <option value="10">10</option>
+                        <option value="20">20</option>
+                    </select>
+                </label>
+            </div>}
+        </div>
+    );
 }
 export default Paginator;
